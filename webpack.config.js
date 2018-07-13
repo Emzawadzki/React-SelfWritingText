09201351dd3fd0e459396fbb90db33c9186e47a7
Michał Zawadzki <emzawadzki@gmail.com>
2018-07-13 23:02:38 +0200

    Testing environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
 var path = require("path");
 module.exports = {
   entry: {
-    main: path.join(__dirname, 'src', 'SelfWritingText.jsx')
+    main: path.join(__dirname, 'example', 'example.jsx')
   },
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname)
+    path: path.resolve(__dirname, 'example')
   },
   mode: "development",
   watch: true,
   module: {
-    rules: [{
-      test: /\.jsx$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: [
-            "es2015",
-            'stage-2',
-            "react"
-          ]
-        }
-      }
-    }]
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "es2015",
+              'stage-2',
+              "react"
+            ]
+          }
+        },
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
   }
 }
