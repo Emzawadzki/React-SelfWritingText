4ceedefe45e67196b5ec5c404997a36758df9e8e
Michał Zawadzki <emzawadzki@gmail.com>
2018-07-13 23:05:54 +0200

    Rendering tests added

---
+++ __tests__/self-writing-text.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Swt from '../src/SelfWritingText.jsx';
+
+describe('Rendering tests:', () => {
+  it('Renders nothing without required props', () => {
+    const wrapper = shallow(<Swt />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Renders correctly with props', () => {
+    const wrapper = shallow(<Swt class={'myclass'} textMajorIn={'testingText'} textIn={['testText1', 'testText2', 'testText3']}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
